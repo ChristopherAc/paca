2149e49b189f6bb513d6774b6f18db3b5c3ad3f2
Sivar Shwan Aziz <siver-king@hotmail.com>
2018-04-28 15:50:31 +0200

    fixat bug med changepassword.

--- paca/paca_app/urls.py
+++ paca/paca_app/urls.py
@@ -4,11 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     # inställningar
-    path('settings', views.settings, name='settings'),
+    path('settings/', views.settings, name='settings'),
     # Meddelande
     path('message', views.message, name='message'),
     # Ändra lösenord ( första inloggningen )
-    path('changepassword', views.change_password, name='changepassword'),
+    path('changepassword/', views.change_password, name='changepassword'),
     # lägg till en arbetsgivare
     path('add_user', views.add_user, name='add_user')
 ]
